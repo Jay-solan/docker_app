fd1ef83fcb2113d2fb4a61a8d576f9c90dd25226
Jay-solan <jaipalsolanki101@gmail.com>
2022-06-19 21:37:00 +0530

    doc added

--- app/app.py
+++ app/app.py
@@ -1,25 +1,46 @@
+"""
+high level support for doing this and that.
+"""
 from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
+
 @app.route('/')
 def default():
+    """
+high level support for doing this and that.
+"""
     return 'Welcome to the website'
+
+
 @app.route('/hello')
 def hello():
+    """
+high level support for doing this and that.
+"""
     return 'Hello World'
+
+
 @app.route('/success/<name>')
 def success(name):
+    """
+high level support for doing this and that.
+"""
     return 'Welcome %s' % name
 
-@app.route('/login',methods = ['POST', 'GET'])
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    """
+high level support for doing this and that.
+"""
     if request.method == 'POST':
         user = request.form['nm']
-        return redirect(url_for('success',name = user))
+        return redirect(url_for('success', name=user))
     else:
         user = request.args.get('nm')
-        return redirect(url_for('success',name = user))
+        return redirect(url_for('success', name=user))
+
 
 if __name__ == '__main__':
-    app.run(debug = True, host="0.0.0.0")
-    
+    app.run(debug=True, host="0.0.0.0")
